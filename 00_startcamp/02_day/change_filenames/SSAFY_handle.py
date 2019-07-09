6e3775ecd5cf7ff6e130fc5ae3424250e74d7665
chajk811 <chajk811@gmail.com>
2019-07-09 17:40:04 +0900

    third commit

---
+++ 00_startcamp/02_day/change_filenames/SSAFY_handle.py
@@ -0,0 +1,16 @@
+import os
+
+# 1. 해당 파일들이 있는 위치로 이동
+os.chdir(r'C:\Users\student\Desktop\TIL\00_startcamp\02_day\change_filenames')
+
+# 2. 현재 폴더 안에 모든 파일 이름을 수집
+filenames = os.listdir('.')
+
+# 3. 각각의 파일명을 돌면서 수정한다.
+# for filename in filenames:
+#     os.rename(filename, f'SAMESUNG_{filename}')
+
+# 4. SAMSUNG을 SSAFY로 변환
+for filename in filenames:
+    os.rename(filename, filename.replace('SAMESUNG_', 'SSAFY_'))
+
