59cd735db9f2c1eb4e54f36a2ffc4648947b1260
chajk811 <chajk811@gmail.com>
2019-07-12 15:53:03 +0900

    2019_07_12 | telegram_bot

--- 00_startcamp/04_day/flask/app.py
+++ 00_startcamp/04_day/flask/app.py
@@ -112,48 +112,52 @@ def result():
 
 #로또 회차/내병호 입력 페이지
 #결과 페이지
+import requests
 
 @app.route('/lotto_check')
 def lotto_check():
     return render_template('lotto_check.html')
 
+
 @app.route('/lotto_result')
 def lotto_result():
     # 회차 번호를 받아온다.
-    num = request.arg.get('num')
+    num = request.args.get('num')
     # 동행복권에 요청을 보내 응답을 받는다.
     res = requests.get(f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}')
-    # 제이슨 형태로 바꿔준다. (우리가 크롬에서 보고있는 결과와 동일한 모습)
-    lotto = res.jason()
-    
+    # json 형태로 바꿔준다. (우리가 크롬에서 보고있는 결과와 동일한 모습)
+    lotto = res.json()
+
     # 당첨번호 6개만 가져오기
-     winner = []
-     for i in range(1, 7):
-          winner.append(lotto[f'drwtNo{i}'])
+    winner = []
+    for i in range(1, 7):
+        winner.append(lotto[f'drwtNo{i}'])
 
-     # 내 번호 리스트 만들기
-     numbers = []
-     for num in request.arg.get('numbers').split():
-          numbers.append(int(num))
+    # 내 번호 리스트 만들기
+    numbers = []
+    for num in request.args.get('numbers').split():
+        numbers.append(int(num))
+    
+    # 등수 가리기(몇개 맞았는지 교집합이 필요)
+    # 내 번호 요소를 뽑아서 당첨번호 리스트에 있는지 확인.
+    matched = 0
+    for num in numbers:
+        if num in winner:
+            matched += 1
 
-     # 등수 가리기(몇개 맞았는지 교집합이 필요)
-     matched = 0
-     # 내 번호 요소를 뽑아서 당첨번호 리스트에 있는지 확인.
-     for num in numbers:
-          if num in winner:
-               matched += 1
-     if matched == 6:
-          result = '1등입니다!'
-     elif matched == 5:
-          if lotto['bnusNo'] in numbers:
-               result = '2등입니다!'
-          else:
-               result = '3등입니다!'
-     elif matched == 4:
-          result = '4등입니다.'
-     elif matched == 3:
-          result = '5등입니다.'
-     else:
-          result = '꽝입니다.'
-     return render_template('lotto_result.html', winner = winner, numbers =numbers, )
-
+    if matched == 6:
+        result = '1등입니다!'
+    elif matched == 5:
+        if lotto['bnusNo'] in numbers:
+            result = '2등입니다!'
+        else:
+            result = '3등입니다!'
+    elif matched == 4:
+        result = '4등입니다!'
+    elif matched == 3:
+        result = '5등입니다!'
+    else:
+        result = '꽝입니다!'
+    
+    return render_template('lotto_result.html', winner=winner, numbers=numbers, result=result)
+    
---
+++ 00_startcamp/telegram_bot/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import requests
+from decouple import config
+
+app = Flask(__name__)
+
+api_url = 'https://api.telegram.org'
+token = config('TELEGRAM_BOT_TOKEN')
+chat_id = config('CHAT_ID')
+naver_client_id = config('NAVER_CLIENT_ID')
+naver_client_secret = config('NAVER_CLIENT_SECRET')
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+
+@app.route(f'/{token}', methods=['POST'])
+def telegram():
+    # 1. 데이터 구조 print 해보기
+    from_telegram = request.get_json()
+
+    if from_telegram.get('message') is not None:
+        chat_id = from_telegram.get('message').get('from').get('id')
+        text = from_telegram.get('message').get('text')
+        
+        # 한글키워드 받기
+
+        # '/번역 ' 으로 입력이 시작된다면, 파파고로 번역이 동작한다.
+        if text[0:4] == '/번역 ':
+            headers = {
+                'X-Naver-Client-Id': naver_client_id,
+                'X-Naver-Client-Secret': naver_client_secret
+            }
+            data = {'source': 'ko', 'target': 'en', 'text': text[4:]}
+            papago_res = requests.post('https://openapi.naver.com/v1/papago/n2mt', headers=headers, data=data)
+            text = papago_res.json().get('message').get('result').get('translatedText') # 여기에 한영 번역 text 가 있다.
+            
+        if text[0:4] == '/로또 ':
+            num = text[4:]
+            # 동행복권에 요청을 보내 응답을 받는다.
+            res = requests.get(f'https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={num}')
+            # json 형태로 바꿔준다. (우리가 크롬에서 보고있는 결과와 동일한 모습)
+            lotto = res.json()
+
+            # 당첨번호 6개만 가져오기
+            winner = []
+            for i in range(1, 7):
+                winner.append(lotto[f'drwtNo{i}'])
+            text = winner
+        requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
+    return '', 200
