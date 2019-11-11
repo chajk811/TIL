883dee91fef4536f15d4a254ac17a4ef9e70568b
chajk811 <chajk811@gmail.com>
2019-11-11 15:51:13 +0900

    2019_11_11 | vue_youtube_pjt

---
+++ 07_vue/04_vue_youtube_project/youtube-browser/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <!-- 만약 inputchange 이벤트가 일어나면 onInputChange 라는 method 가 실행된 -->
+    <search-bar @inputChange="onInputChange"></search-bar>
+    <div class="row">
+      <video-detail :video="selectedVideo"></video-detail>
+      <video-list @videoSelect="onVideoSelect" :videos="videos"></video-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
+
+const API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY
+const API_URL = 'https://www.googleapis.com/youtube/v3/search'
+
+export default {
+  name: 'App', //최상단 컴포넌트기 때문에 이름이 없어도 되지만 명시적으로 작성한다.
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null,
+    }
+  },
+  components: {
+    SearchBar,
+    VideoList,
+    VideoDetail,
+  },
+  methods: {
+    onVideoSelect(video) {
+      this.selectedVideo = video
+    },
+    onInputChange(inputValue) {
+      axios.get(API_URL, {
+        params: {
+          key: API_KEY,
+          type: 'video',
+          part: 'snippet',
+          q: inputValue,
+        }
+      })
+      .then(response => {
+        this.videos = response.data.items
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ 07_vue/04_vue_youtube_project/youtube-browser/src/components/VideoListItem.vue
@@ -0,0 +1,39 @@
+<template>
+	<li @click="onVideoSelect" class="list-group-item">
+		<img :src="thumbnailUrl" alt="img">
+		<div v-html="video.snippet.title" class="media-body"></div>
+	</li>
+</template>
+
+<script>
+	export default {
+		name: 'VideoListItem',
+		props: {
+			video:{
+				type: Object,
+				required: true,
+			},
+		},
+		methods: {
+			onVideoSelect() {
+				this.$emit('videoSelect', this.video)
+			}
+		},
+		computed:{
+			thumbnailUrl() {
+				return this.video.snippet.thumbnails.default.url
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	li {
+		display: flex;
+		cursor: pointer;
+	}
+
+	li:hover {
+		background-color: gainsboro;
+	}
+</style>
